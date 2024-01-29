72b7ff43f5e20e40e49072a8874ebd4092a0c303
chenyk1219 <150651687+chenyk1219@users.noreply.github.com>
2024-01-29 14:47:52 +0800

    更新网络信息Panel显示

--- Modules/ip-info/ip-info.js
+++ Modules/ip-info/ip-info.js
@@ -11,7 +11,7 @@ function getIpInfo() {
         if (v6?.primaryRouter && getSSID()) info.push(`IPv6地址：已分配`);
     }
     // info = info.join("\n");
-    return info.join("\n") + '\n\n';
+    return info.join("\n") + '\n';
 }
 
 function getFlagEmoji(countryCode) {
