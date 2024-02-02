bce34a30990fdf1f47ab33fbb7197b89e8ea12ed
chenyk1219 <150651687+chenyk1219@users.noreply.github.com>
2024-02-02 11:29:37 +0800

    整合UnBan.list
    将目前所知的DivineEngine、ACL4SSR、blackmatrix7、Centralmatrix3的Unbreak.list整合到
    
    https://raw.githubusercontent.com/chenyk1219/surge/release/UnBreak.list
    
    每天凌晨自动更新，有新的UnBreak.list需要整合可以联系我

--- install.py
+++ install.py
@@ -45,9 +45,30 @@ def get_china_ip():
             continue
 
 
+# 获取规则修正
+def unbreak():
+    unbreak_filename = os.path.join('dist', 'UnBreak.list')
+    unbreak_set = set()
+    unbreak_url_list = ['https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/Unbreak.list',
+                        'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/UnBan.list',
+                        'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Direct/Direct.list',
+                        'https://raw.githubusercontent.com/Centralmatrix3/Matrix-io/master/Surge/Ruleset/Unbreak.list']
+
+    for unbreak_url in unbreak_url_list:
+        unbreak_list = requests.request('GET', unbreak_url).text.split('\n')
+        for unbreak in unbreak_list:
+            if not unbreak.startswith('#'):
+                unbreak_set.add(unbreak) if unbreak.find('/') == -1 else unbreak_set.add(unbreak.split('/')[0])
+    with open(unbreak_filename, 'wb') as file:
+        for unbreak_url in unbreak_set:
+            unbreak_url = unbreak_url + '\n'
+            file.write(unbreak_url.encode())
+
+
 if __name__ == '__main__':
     if not os.path.exists("dist"):
         os.makedirs("dist")
 
     get_china_ip()
     get_china_domain()
+    unbreak()
